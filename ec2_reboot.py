d5a883af1f2a0c2326147fcbccd06d52aa34423f
Chris <Chris.Bachmeyer@gmail.com>
2020-05-20 20:05:22 -0600

    changes

--- ec2_reboot.py
+++ ec2_reboot.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python3
 
-import boto3
+import boto3, sys
 ec2 = boto3.resource('ec2')
 
 for id in sys.argv[1:]:
-    instance = ec2.instance(id)
+    instance = ec2.Instance(id)
     print(instance.reboot())
