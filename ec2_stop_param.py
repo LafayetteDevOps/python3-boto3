47f48219b225782c8e38d4a930f5831c74cb599b
Christopher Bachmeyer <Chris.Bachmeyer@gmail.com>
2020-05-31 17:39:59 -0600

    adjusted instance ids in each script

--- ec2_stop_param.py
+++ ec2_stop_param.py
@@ -3,7 +3,7 @@
 import boto3
 ec2 = boto3.resource('ec2')
 
-instances = ['i-02b597fa65beb3880','i-0391f89d9c136b9bd']
+instances = ['i-0d3ca60ac96e8a896']
 
 for id in instances:
     instance = ec2.Instance(id)
